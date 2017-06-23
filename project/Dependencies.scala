b656630da198501753f9dd1334a2ebbf8b79e285
megrez <lujiajing1126@gmail.com>
2017-06-23 10:35:27 +0800

    bump akka version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ object Dependencies {
     val slf4j = "1.7.24"
     val logback = "1.2.1"
     val scalaTest = "3.0.1"
-    val akka = "2.4.18"
+    val akka = "2.5.3"
     val kafka = "0.10.2.1"
     val typesafeConfig = "1.3.1"
   }
